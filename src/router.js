07901a5fbb4dd14bbed4aeb28318e90573f9ab36
Circle Lin <nilcric@outlook.com>
2019-05-19 09:56:49 +0800

    锚点

--- src/router.js
+++ src/router.js
@@ -5,22 +5,44 @@ Vue.use(Router)
 
 export default new Router({
     mode: 'history',
+    // eslint-disable-next-line
+    scrollBehavior(to, from, savedPosition) {
+        if (to.hash) {
+            return {
+                selector: to.hash,
+                offset: {
+                    x: 0,
+                    y: 64,
+                }
+            }
+        } else {
+            return {
+                x: 0,
+                y: 0,
+            }
+        }
+    },
     base: process.env.BASE_URL,
     routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: () => import('./components/Home.vue')
+        },
         {
             path: '/damage',
             name: 'Damage',
             component: () => import('./components/Damage.vue')
         },
         {
-            path: '/random',
-            name: 'Random',
-            component: () => import('./components/Random.vue')
-        },
-        {
             path: '/order',
             name: 'Order',
             component: () => import('./components/Order.vue')
+        },
+        {
+            path: '/probability',
+            name: 'Probability',
+            component: () => import('./components/Probability.vue')
         },
         {
             path: '/status',
